7d71adc91c50a399ba2ce694b61916db04a7a426
leos1999 <101leosabraham@gmail.com>
2019-11-23 11:35:09 +0530

    marshmallow issue resolved

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask,request,jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
+# Init app
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def get():
+    return jsonify({'msg':'Hello World'})
+#Run the server 
+if __name__ == "__main__":
+    app.run(debug=True)
